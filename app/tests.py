bf8c9d01e2e7f8dda5e53bfff2d6802c29b9ac8d
App Generator <support@appseed.us>
2021-09-09 09:40:29 +0300

    CleanUP before Update

--- app/tests.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
-from django.test import TestCase
-
-# Create your tests here.
--- authentication/config.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
-from django.apps import AppConfig
-
-class AuthConfig(AppConfig):
-    name = 'authcfg'
