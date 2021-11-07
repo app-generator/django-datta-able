488bc7c304e7ca11e11d0e9163f28f8146772051
App Generator <support@appseed.us>
2021-11-07 12:24:01 +0200

    CleanUp before Update

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn core.wsgi --log-file=- 
--- apps/authentication/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
-from django.contrib import admin
-
-# Register your models here.
