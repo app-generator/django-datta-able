f6d283ef5da761a637362490f35b440347bf8c8b
App Generator <51070104+app-generator@users.noreply.github.com>
2023-06-28 15:19:52 +0300

    Added Product MODEL

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    id    = models.AutoField(primary_key=True)
+    name  = models.CharField(max_length = 100) 
+    info  = models.CharField(max_length = 100, default = '')
+    info2 = models.CharField(max_length = 100, default = '')
+    info3 = models.CharField(max_length = 100, default = '')
+    info4 = models.CharField(max_length = 100, default = '')
+
+    def __str__(self):
+        return self.name
