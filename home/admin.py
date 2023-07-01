7d7c37c5c3668b3ccb88f4faaf491af3a7644ade
App Generator <51070104+app-generator@users.noreply.github.com>
2023-07-01 17:24:18 +0300

    Home.Admin - Automatic Models Discovery

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from django.apps import apps
+from django.contrib import admin
+
 # Register your models here.
+
+app_models = apps.get_app_config('home').get_models()
+for model in app_models:
+    try:    
+
+        admin.site.register(model)
+
+    except Exception:
+        pass
