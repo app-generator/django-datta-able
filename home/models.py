6129bd492bd039ee1f04dfc3206aa68d0434d240
App Generator <51070104+app-generator@users.noreply.github.com>
2023-07-01 16:24:23 +0300

    Models - Update PRODUCT

--- home/models.py
+++ home/models.py
@@ -6,9 +6,7 @@ class Product(models.Model):
     id    = models.AutoField(primary_key=True)
     name  = models.CharField(max_length = 100) 
     info  = models.CharField(max_length = 100, default = '')
-    info2 = models.CharField(max_length = 100, default = '')
-    info3 = models.CharField(max_length = 100, default = '')
-    info4 = models.CharField(max_length = 100, default = '')
+    price = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
