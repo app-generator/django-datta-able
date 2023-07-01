f7842ab74ea7d97c967016c5c7ef88017bd712e0
App Generator <51070104+app-generator@users.noreply.github.com>
2023-07-01 17:24:45 +0300

    URLs - Added API GEN

--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token # <-- NEW
 
 urlpatterns = [
     path('', include('home.urls')),
     path("admin/", admin.site.urls),
     path("", include('admin_datta.urls')),
-    path('', include('django_dyn_dt.urls')),  # <-- NEW: Dynamic_DT Routing 
+    path('', include('django_dyn_dt.urls')), # <-- NEW: Dynamic_DT Routing   
 ]
+
+# Lazy-load on routing is needed
+# During the first build, API is not yet generated
+try:
+    urlpatterns.append( path("api/"      , include("api.urls"))    )
+    urlpatterns.append( path("login/jwt/", view=obtain_auth_token) )
+except:
+    pass
