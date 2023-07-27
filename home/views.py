8ea47af6e7c80d6283fbcd5e5f0cb613d7c22f9a
App Generator <51070104+app-generator@users.noreply.github.com>
2023-07-27 16:01:38 +0300

    Update views.py

--- home/views.py
+++ home/views.py
@@ -6,15 +6,13 @@ from django.contrib.auth import logout
 
 from django.contrib.auth.decorators import login_required
 
-from .models import Site
+from .models import *
 
 def index(request):
 
-  units = Site.objects.all().values()
-
   context = {
-    'segment': 'index',
-    'units'  : units    
+    'segment'  : 'index',
+    #'products' : Product.objects.all()
   }
   return render(request, "pages/index.html", context)
 
@@ -22,136 +20,4 @@ def tables(request):
   context = {
     'segment': 'tables'
   }
-  return render(request, "pages/tables.html", context)
-
-# Components
-@login_required(login_url='/accounts/login/')
-def bc_button(request):
-  context = {
-    'parent': 'basic_components',
-    'segment': 'button'
-  }
-  return render(request, "pages/components/bc_button.html", context)
-
-@login_required(login_url='/accounts/login/')
-def bc_badges(request):
-  context = {
-    'parent': 'basic_components',
-    'segment': 'badges'
-  }
-  return render(request, "pages/components/bc_badges.html", context)
-
-@login_required(login_url='/accounts/login/')
-def bc_breadcrumb_pagination(request):
-  context = {
-    'parent': 'basic_components',
-    'segment': 'breadcrumbs_&_pagination'
-  }
-  return render(request, "pages/components/bc_breadcrumb-pagination.html", context)
-
-@login_required(login_url='/accounts/login/')
-def bc_collapse(request):
-  context = {
-    'parent': 'basic_components',
-    'segment': 'collapse'
-  }
-  return render(request, "pages/components/bc_collapse.html", context)
-
-@login_required(login_url='/accounts/login/')
-def bc_tabs(request):
-  context = {
-    'parent': 'basic_components',
-    'segment': 'navs_&_tabs'
-  }
-  return render(request, "pages/components/bc_tabs.html", context)
-
-@login_required(login_url='/accounts/login/')
-def bc_typography(request):
-  context = {
-    'parent': 'basic_components',
-    'segment': 'typography'
-  }
-  return render(request, "pages/components/bc_typography.html", context)
-
-@login_required(login_url='/accounts/login/')
-def icon_feather(request):
-  context = {
-    'parent': 'basic_components',
-    'segment': 'feather_icon'
-  }
-  return render(request, "pages/components/icon-feather.html", context)
-
-
-# Forms and Tables
-@login_required(login_url='/accounts/login/')
-def form_elements(request):
-  context = {
-    'parent': 'form_components',
-    'segment': 'form_elements'
-  }
-  return render(request, 'pages/form_elements.html', context)
-
-@login_required(login_url='/accounts/login/')
-def basic_tables(request):
-  context = {
-    'parent': 'tables',
-    'segment': 'basic_tables'
-  }
-  return render(request, 'pages/tbl_bootstrap.html', context)
-
-# Chart and Maps
-@login_required(login_url='/accounts/login/')
-def morris_chart(request):
-  context = {
-    'parent': 'chart',
-    'segment': 'morris_chart'
-  }
-  return render(request, 'pages/chart-morris.html', context)
-
-@login_required(login_url='/accounts/login/')
-def google_maps(request):
-  context = {
-    'parent': 'maps',
-    'segment': 'google_maps'
-  }
-  return render(request, 'pages/map-google.html', context)
-
-# Authentication
-class UserRegistrationView(CreateView):
-  template_name = 'accounts/auth-signup.html'
-  form_class = RegistrationForm
-  success_url = '/accounts/login/'
-
-class UserLoginView(LoginView):
-  template_name = 'accounts/auth-signin.html'
-  form_class = LoginForm
-
-class UserPasswordResetView(PasswordResetView):
-  template_name = 'accounts/auth-reset-password.html'
-  form_class = UserPasswordResetForm
-
-class UserPasswrodResetConfirmView(PasswordResetConfirmView):
-  template_name = 'accounts/auth-password-reset-confirm.html'
-  form_class = UserSetPasswordForm
-
-class UserPasswordChangeView(PasswordChangeView):
-  template_name = 'accounts/auth-change-password.html'
-  form_class = UserPasswordChangeForm
-
-def logout_view(request):
-  logout(request)
-  return redirect('/accounts/login/')
-
-@login_required(login_url='/accounts/login/')
-def profile(request):
-  context = {
-    'segment': 'profile',
-  }
-  return render(request, 'pages/profile.html', context)
-
-@login_required(login_url='/accounts/login/')
-def sample_page(request):
-  context = {
-    'segment': 'sample_page',
-  }
-  return render(request, 'pages/sample-page.html', context)
+  return render(request, "pages/dynamic-tables.html", context)
