ec3dde79dff881d5040a4e5ef8e487969dc12597
Uladzislau Saldatsenka <midnightroam1@gmail.com>
2023-08-08 16:49:25 +0200

    Refactored the views file in the home directory. Written documentation for views and in general views have been rewritten using class based views. urls.py has also been tweaked to fit the new code.

--- home/views.py
+++ home/views.py
@@ -1,23 +1,80 @@
 from django.shortcuts import render, redirect
 from admin_datta.forms import RegistrationForm, LoginForm, UserPasswordChangeForm, UserPasswordResetForm, UserSetPasswordForm
 from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordResetConfirmView, PasswordResetView
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.contrib.auth import logout
 
 from django.contrib.auth.decorators import login_required
 
 from .models import *
 
-def index(request):
 
-  context = {
-    'segment'  : 'index',
-    #'products' : Product.objects.all()
-  }
-  return render(request, "pages/index.html", context)
+class IndexTemplateView(TemplateView):
+    """
+    A class-based view to render the index page template.
 
-def tables(request):
-  context = {
-    'segment': 'tables'
-  }
-  return render(request, "pages/dynamic-tables.html", context)
+    Attributes:
+        template_name (str): The name of the template to be rendered ('pages/index.html').
+
+    Methods:
+        get_context_data(**kwargs):
+            Retrieves the context data for rendering the template.
+            Adds a context variable 'segment' with the value 'index'.
+            Args:
+                **kwargs: Additional keyword arguments.
+            Returns:
+                dict: A dictionary containing the context data for the template.
+    """
+    template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs):
+        """
+        Retrieves the context data for rendering the template.
+
+        Adds a context variable 'segment' with the value 'index'.
+
+        Args:
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            dict: A dictionary containing the context data for the template.
+        """
+        context = super().get_context_data(**kwargs)
+        context["segment"] = 'index'
+        return context
+
+
+class TablesTemplateView(TemplateView):
+    """
+    A class-based view to render the dynamic tables page template.
+
+    Attributes:
+        template_name (str): The name of the template to be rendered ('pages/dynamic-tables.html').
+
+    Methods:
+        get_context_data(**kwargs):
+            Retrieves the context data for rendering the template.
+            Adds a context variable 'segment' with the value 'tables'.
+            Args:
+                **kwargs: Additional keyword arguments.
+            Returns:
+                dict: A dictionary containing the context data for the template.
+    """
+    template_name = 'pages/dynamic-tables.html'
+
+    def get_context_data(self, **kwargs):
+        """
+        Retrieves the context data for rendering the template.
+
+        Adds a context variable 'segment' with the value 'tables'.
+
+        Args:
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            dict: A dictionary containing the context data for the template.
+        """
+        context = super().get_context_data(**kwargs)
+        context["segment"] = 'tables'
+        return context
+
