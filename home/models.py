b96f961b01c4ed82d088eff9bd8e2d20a14d1027
Sreejith C <161854226+NerdSreejith@users.noreply.github.com>
2024-03-16 20:07:28 +0530

    some

--- home/models.py
+++ home/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Product(models.Model):
-    id    = models.AutoField(primary_key=True)
-    name  = models.CharField(max_length = 100) 
-    info  = models.CharField(max_length = 100, default = '')
-    price = models.IntegerField(blank=True, null=True)
+# class Product(models.Model):
+#     id    = models.AutoField(primary_key=True)
+#     name  = models.CharField(max_length = 100) 
+#     info  = models.CharField(max_length = 100, default = '')
+#     price = models.IntegerField(blank=True, null=True)
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
    
