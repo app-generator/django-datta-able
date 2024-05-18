9bd8430cdb437daa1a75b5b2a71b20c534a2dd48
David <david@trave.io>
2024-05-18 17:45:07 +0300

    chore: Add DHL API credentials and Shipment model

--- home/models.py
+++ home/models.py
@@ -4,10 +4,15 @@ from django.db import models
 
 class Product(models.Model):
     id    = models.AutoField(primary_key=True)
-    name  = models.CharField(max_length = 100) 
+    name  = models.CharField(max_length = 100)
     info  = models.CharField(max_length = 100, default = '')
     price = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
-   
+
+
+class Shipment(models.Model):
+    recipient_address = models.JSONField()
+    package_details = models.JSONField()
+    tracking_number = models.CharField(max_length=50)
